0f16846c594ebf942181bfa91308a3e67d40870b
Oliinyk Maksym <maxim@oliinyk.com.ua>
2025-02-21 12:30:42 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import asyncio
 from blockwatcher.subscriptions.pending_tx_subscription import sub_manager
+from blockwatcher.database.db_manager import DatabaseManager
+from config import DB_URL
 
 async def main():
-    """Main entry point for the BlockWatcher application."""
-    await sub_manager()
-
-if __name__ == "__main__":
+    db_manager = DatabaseManager(DB_URL)
+    await db_manager.connect()
+    await db_manager.create_tables()
     try:
-        asyncio.run(main())
+        await sub_manager(db_manager)
     except KeyboardInterrupt:
         print("\n🛑 Stopped by user.")
+    except asyncio.CancelledError:
+        print("\n⚠️ Task was cancelled.")
     except Exception as e:
         print(f"⚠️ An error occurred: {e}")
+    finally:
+        await db_manager.close()
+    
+if __name__ == "__main__":
+    asyncio.run(main())
