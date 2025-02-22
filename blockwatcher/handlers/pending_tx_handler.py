079db0db0cc38c60defdf9ca41d20303fe582011
Oliinyk Maksym <maxim@oliinyk.com.ua>
2025-02-22 16:28:56 -0500

    Updated handler

--- blockwatcher/handlers/pending_tx_handler.py
+++ blockwatcher/handlers/pending_tx_handler.py
@@ -1,28 +1,23 @@
 from web3.utils.subscriptions import PendingTxSubscriptionContext
 from web3.exceptions import ContractLogicError
+from blockwatcher.utils.utils import fetch_token_metadata
 from config import ERC20_ABI, ERC20_TRANSFER_SIGNATURE, ERC20_TRANSFER_FROM_SIGNATURE 
 
-async def pending_tx_handler(handler_context: PendingTxSubscriptionContext, w3, db_manager):
+async def pending_tx_handler(handler_context, w3, db_manager):
     tx_hash = '0x' + handler_context.result.hex()
     try:
         tx = await w3.eth.get_transaction(tx_hash)
         tx_input = "0x"+tx['input'].hex()
 
         if tx['value']>0:
-            token = "NATIVE"
-            from_address = tx['from']
-            to_address = tx['to']
-            value = tx['value']
-            gas = tx['gas']
-            gas_price = tx['gasPrice']
             print(f"💰 Native ETH Transfer Detected\n"
-                  f"From: {from_address}\n"
-                  f"To: {to_address}\n"
-                  f"Value: {value}\n"
-                  f"Gas: {gas}\n"
-                  f"Gas Price: {gas_price}\n"
+                  f"From: {tx['from']}\n"
+                  f"To: {tx['to']}\n"
+                  f"Value: {tx['value']}\n"
+                  f"Gas: {tx['gas']}\n"
+                  f"Gas Price: {tx['gasPrice']}\n"
                   f"Hash: {tx_hash}")
-            await db_manager.store_pending_tx(tx_hash, token, from_address, to_address, value, gas, gas_price)
+            await db_manager.store_pending_tx(tx_hash, "NATIVE", tx['from'], tx['to'], tx['value'], tx['gas'], tx['gasPrice'])
             print('-'*75)
             print('\n')
 
@@ -31,40 +26,34 @@ async def pending_tx_handler(handler_context: PendingTxSubscriptionContext, w3, 
             method, args = contract.decode_function_input(tx_input)
 
             if method.fn_name == "transfer":
-                token = tx['to']
-                from_address = tx['from']
-                to_address = args['to']
-                value = args['value']
-                gas = tx['gas']
-                gas_price = tx['gasPrice']
+                token_address, name, symbol, decimals = await fetch_token_metadata(w3, tx['to'])
+                await db_manager.store_token_metadata(token_address, name, symbol, decimals)
+
                 print(f"🦈 Whale Transfer Detected\n"
-                      f"Token: {token}\n"
-                      f"From: {from_address}\n"
-                      f"To: {to_address}\n"
-                      f"Value: {value}\n"
-                      f"Gas: {gas}\n"
-                      f"Gas Price: {gas_price}\n"
+                      f"Token: {tx['to']}\n"
+                      f"From: {tx['from']}\n"
+                      f"To: {args['to']}\n"
+                      f"Value: {args['value']}\n"
+                      f"Gas: {tx['gas']}\n"
+                      f"Gas Price: {tx['gasPrice']}\n"
                       f"Hash: {tx_hash}")
-                await db_manager.store_pending_tx(tx_hash, token, from_address, to_address, value, gas, gas_price)
+                await db_manager.store_pending_tx(tx_hash, tx['to'], tx['from'], args['to'], args['value'], tx['gas'], tx['gasPrice'])
                 print('-'*75)
                 print('\n')
             
             elif method.fn_name == "transferFrom":
-                token = tx['to']
-                from_address = args['from']
-                to_address = args['to']
-                value = args['value']
-                gas = tx['gas']
-                gas_price = tx['gasPrice']
+                token_address, name, symbol, decimals = await fetch_token_metadata(w3, tx['to'])
+                await db_manager.store_token_metadata(token_address, name, symbol, decimals)
+
                 print(f"🦈 Whale Transfer Detected\n"
-                      f"Token: {token}\n"
-                      f"From: {from_address}\n"
-                      f"To: {to_address}\n"
-                      f"Value: {value}\n"
-                      f"Gas: {gas}\n"
-                      f"Gas Price: {gas_price}\n"
+                      f"Token: {tx['to']}\n"
+                      f"From: {args['from']}\n"
+                      f"To: {args['to']}\n"
+                      f"Value: {args['value']}\n"
+                      f"Gas: {tx['gas']}\n"
+                      f"Gas Price: {tx['gasPrice']}\n"
                       f"Hash: {tx_hash}")
-                await db_manager.store_pending_tx(tx_hash, token, from_address, to_address, value, gas, gas_price)
+                await db_manager.store_pending_tx(tx_hash, tx['to'], args['from'], args['to'], args['value'], tx['gas'], tx['gasPrice'])
                 print('-'*75)
                 print('\n')
 
