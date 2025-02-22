578d1df128b78bfedf77f845181952ae791d3d7c
Oliinyk Maksym <maxim@oliinyk.com.ua>
2025-02-22 17:04:00 -0500

    Refactoring

--- blockwatcher/handlers/pending_tx_handler.py
+++ blockwatcher/handlers/pending_tx_handler.py
@@ -1,9 +1,6 @@
-from web3.utils.subscriptions import PendingTxSubscriptionContext
 from web3.exceptions import ContractLogicError
-from blockwatcher.utils.utils import fetch_token_metadata
-from config import ERC20_ABI, ERC20_TRANSFER_SIGNATURE, ERC20_TRANSFER_FROM_SIGNATURE 
 
-async def pending_tx_handler(handler_context, w3, db_manager):
+async def pending_native_tx_handler(handler_context, w3, db_manager):
     tx_hash = '0x' + handler_context.result.hex()
     try:
         tx = await w3.eth.get_transaction(tx_hash)
@@ -20,42 +17,6 @@ async def pending_tx_handler(handler_context, w3, db_manager):
             await db_manager.store_pending_tx(tx_hash, "NATIVE", tx['from'], tx['to'], tx['value'], tx['gas'], tx['gasPrice'])
             print('-'*75)
             print('\n')
-
-        elif tx_input.startswith(ERC20_TRANSFER_SIGNATURE) or tx_input.startswith(ERC20_TRANSFER_FROM_SIGNATURE):
-            contract = w3.eth.contract(address=tx['to'], abi=ERC20_ABI)
-            method, args = contract.decode_function_input(tx_input)
-
-            if method.fn_name == "transfer":
-                token_address, name, symbol, decimals = await fetch_token_metadata(w3, tx['to'])
-                await db_manager.store_token_metadata(token_address, name, symbol, decimals)
-
-                print(f"🦈 Whale Transfer Detected\n"
-                      f"Token: {tx['to']}\n"
-                      f"From: {tx['from']}\n"
-                      f"To: {args['to']}\n"
-                      f"Value: {args['value']}\n"
-                      f"Gas: {tx['gas']}\n"
-                      f"Gas Price: {tx['gasPrice']}\n"
-                      f"Hash: {tx_hash}")
-                await db_manager.store_pending_tx(tx_hash, tx['to'], tx['from'], args['to'], args['value'], tx['gas'], tx['gasPrice'])
-                print('-'*75)
-                print('\n')
-            
-            elif method.fn_name == "transferFrom":
-                token_address, name, symbol, decimals = await fetch_token_metadata(w3, tx['to'])
-                await db_manager.store_token_metadata(token_address, name, symbol, decimals)
-
-                print(f"🦈 Whale Transfer Detected\n"
-                      f"Token: {tx['to']}\n"
-                      f"From: {args['from']}\n"
-                      f"To: {args['to']}\n"
-                      f"Value: {args['value']}\n"
-                      f"Gas: {tx['gas']}\n"
-                      f"Gas Price: {tx['gasPrice']}\n"
-                      f"Hash: {tx_hash}")
-                await db_manager.store_pending_tx(tx_hash, tx['to'], args['from'], args['to'], args['value'], tx['gas'], tx['gasPrice'])
-                print('-'*75)
-                print('\n')
 
     except ContractLogicError as e:
         print(f"⚠️ Contract error for transaction {tx_hash}: {e}")
--- blockwatcher/subscriptions/pending_tx_subscription.py
+++ blockwatcher/subscriptions/pending_tx_subscription.py
@@ -1,6 +1,8 @@
 from web3 import AsyncWeb3
 from web3.utils.subscriptions import PendingTxSubscription
-from blockwatcher.handlers.pending_tx_handler import pending_tx_handler
+from blockwatcher.handlers.pending_tx_handler import pending_native_tx_handler
+from blockwatcher.handlers.erc20_pending_tx_handler import pending_erc20_tx_handler
+from blockwatcher.handlers.erc20_handler import erc20_handler
 from blockwatcher.database.db_manager import DatabaseManager
 from config import WSS_URL
 
@@ -12,12 +14,22 @@ async def sub_manager(db_manager):
     if await w3.is_connected():
         print("✅ Connected to Ethereum WebSocket!")
 
-        pending_tx = PendingTxSubscription(
-            label="pending-tx-mainnet",
+        pending_native_tx = PendingTxSubscription(
+            label="pending-tx",
             full_transactions=True,
-            handler=lambda ctx: pending_tx_handler(ctx, w3, db_manager))
+            handler=lambda ctx: pending_native_tx_handler(ctx, w3, db_manager))
+        
+        pending_erc20_tx = PendingTxSubscription(
+            label="pending-erc20-tx",
+            full_transactions=True,
+            handler=lambda ctx: pending_erc20_tx_handler(ctx, w3, db_manager))
+        
+        erc20_token = PendingTxSubscription(
+            label="erc20-tokens",
+            full_transactions=True,
+            handler=lambda ctx: erc20_handler(ctx, w3, db_manager))
 
-        await w3.subscription_manager.subscribe([pending_tx])
+        await w3.subscription_manager.subscribe([pending_native_tx, pending_erc20_tx, erc20_token])
         await w3.subscription_manager.handle_subscriptions()
     else:
         print("❌ Connection failed. Check WSS URL.")
