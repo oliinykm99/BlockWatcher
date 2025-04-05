e9aca31c52402f4107ba4a0308e2e71720ff690e
Oliinyk Maksym <maxim@oliinyk.com.ua>
2025-04-05 16:48:01 -0400

    Added separate ERC20 price handler

--- blockwatcher/handlers/__init__.py
+++ blockwatcher/handlers/__init__.py
@@ -1,8 +1,10 @@
 from .erc20_handler import erc20_handler
 from .erc20_pending_tx_handler import pending_erc20_tx_handler
 from .pending_tx_handler import pending_native_tx_handler
+from .erc20_price_handler import erc20_price_handler
 
 
 __all__ = ["erc20_handler",
            "pending_erc20_tx_handler",
-           "pending_native_tx_handler"]
+           "pending_native_tx_handler",
+           "erc20_price_handler"]
