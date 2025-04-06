c202b725bed654c48755e76e588636adafe9b634
Oliinyk Maksym <maxim@oliinyk.com.ua>
2025-04-06 12:40:56 -0400

    Queue management

--- blockwatcher/subscriptions/pending_tx_subscription.py
+++ blockwatcher/subscriptions/pending_tx_subscription.py
@@ -1,36 +1,33 @@
+import asyncio
 from web3.utils.subscriptions import PendingTxSubscription
 from blockwatcher.handlers import pending_native_tx_handler, pending_erc20_tx_handler, erc20_handler, erc20_price_handler
 from blockwatcher.utils import ws_manager
 
+async def run_subscription(label, handler, db_manager):
+    """Run a single subscription."""
+    w3 = ws_manager.w3
+    subscription = PendingTxSubscription(
+        label=label,
+        full_transactions=True,
+        handler=lambda ctx: handler(ctx, db_manager)
+    )
+    await w3.subscription_manager.subscribe([subscription])
+    await w3.subscription_manager.handle_subscriptions()
+
 async def sub_manager(db_manager):
-    """Sets up subscriptions for pending transactions."""
+    """Starts independent workers for each subscription."""
     await ws_manager.connect()
 
     if await ws_manager.is_connected():
         print("✅ Connected to Ethereum WebSocket!")
-        w3 = ws_manager.w3
 
-        pending_native_tx = PendingTxSubscription(
-            label="pending-tx",
-            full_transactions=True,
-            handler=lambda ctx: pending_native_tx_handler(ctx, db_manager))
-        
-        pending_erc20_tx = PendingTxSubscription(
-            label="pending-erc20-tx",
-            full_transactions=True,
-            handler=lambda ctx: pending_erc20_tx_handler(ctx, db_manager))
-        
-        erc20_token = PendingTxSubscription(
-            label="erc20-tokens",
-            full_transactions=True,
-            handler=lambda ctx: erc20_handler(ctx, db_manager))
-        
-        erc20_token_price = PendingTxSubscription(
-            label="erc20-tokens-price",
-            full_transactions=True,
-            handler=lambda ctx: erc20_price_handler(ctx, db_manager))
+        tasks = [
+            asyncio.create_task(run_subscription("pending-tx", pending_native_tx_handler, db_manager)),
+            asyncio.create_task(run_subscription("pending-erc20-tx", pending_erc20_tx_handler, db_manager)),
+            asyncio.create_task(run_subscription("erc20-tokens", erc20_handler, db_manager)),
+            #asyncio.create_task(run_subscription("erc20-tokens-price", erc20_price_handler, db_manager)),
+        ]
 
-        await w3.subscription_manager.subscribe([pending_native_tx, pending_erc20_tx, erc20_token, erc20_token_price])
-        await w3.subscription_manager.handle_subscriptions()
+        await asyncio.gather(*tasks)
     else:
         print("❌ Connection failed. Check WSS URL.")
