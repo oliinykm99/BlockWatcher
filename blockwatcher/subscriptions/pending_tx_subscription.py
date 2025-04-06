7071f206ff071a617e6c621a3b91160634965643
Oliinyk Maksym <maxim@oliinyk.com.ua>
2025-04-06 00:17:09 -0400

    Updated sub_manager

--- blockwatcher/subscriptions/pending_tx_subscription.py
+++ blockwatcher/subscriptions/pending_tx_subscription.py
@@ -1,8 +1,10 @@
+import asyncio
 from web3.utils.subscriptions import PendingTxSubscription
-from blockwatcher.handlers import pending_native_tx_handler, pending_erc20_tx_handler, erc20_handler, erc20_price_handler
+from blockwatcher.telegram import telegram_bot_handler
+from blockwatcher.telegram import telegram_native_alert_handler
 from blockwatcher.utils import ws_manager
 
-async def sub_manager(db_manager):
+async def sub_manager():
     """Sets up subscriptions for pending transactions."""
     await ws_manager.connect()
 
@@ -10,27 +12,24 @@ async def sub_manager(db_manager):
         print("✅ Connected to Ethereum WebSocket!")
         w3 = ws_manager.w3
 
-        pending_native_tx = PendingTxSubscription(
-            label="pending-tx",
-            full_transactions=True,
-            handler=lambda ctx: pending_native_tx_handler(ctx, w3, db_manager))
-        
-        pending_erc20_tx = PendingTxSubscription(
-            label="pending-erc20-tx",
-            full_transactions=True,
-            handler=lambda ctx: pending_erc20_tx_handler(ctx, w3, db_manager))
-        
-        erc20_token = PendingTxSubscription(
-            label="erc20-tokens",
-            full_transactions=True,
-            handler=lambda ctx: erc20_handler(ctx, w3, db_manager))
-        
-        erc20_token_price = PendingTxSubscription(
-            label="erc20-tokens-price",
-            full_transactions=True,
-            handler=lambda ctx: erc20_price_handler(ctx, w3, db_manager))
+        asyncio.create_task(monitor_subscription_queue(w3))
 
-        await w3.subscription_manager.subscribe([pending_native_tx, pending_erc20_tx, erc20_token, erc20_token_price])
+        telegram_native_alert = PendingTxSubscription(
+            label="telegram-native-alert",
+            full_transactions=True,
+            handler=lambda ctx: telegram_native_alert_handler(ctx, telegram_bot_handler))
+
+        await w3.subscription_manager.subscribe([telegram_native_alert])
         await w3.subscription_manager.handle_subscriptions()
     else:
         print("❌ Connection failed. Check WSS URL.")
+
+async def monitor_subscription_queue(w3):
+    while True:
+        try:
+            queue = w3.provider._request_processor._handler_subscription_queue
+            size = queue.qsize()
+            print(f"📊 Subscription Queue Size: {size}")
+        except Exception as e:
+            print(f"❌ Error monitoring subscription queue: {e}")
+        await asyncio.sleep(5)
