b22cf583df9e766f623e6a87de9f2f0736a374db
Oliinyk Maksym <maxim@oliinyk.com.ua>
2025-04-06 02:42:54 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import asyncio
 from blockwatcher.subscriptions import sub_manager
 from blockwatcher.database import DatabaseManager
+from blockwatcher.telegram import telegram_bot_handler
 from config import DB_URL
 
 async def main():
+    await telegram_bot_handler.start()
     db_manager = DatabaseManager(DB_URL)
     await db_manager.connect()
-    await db_manager.create_tables()
     try:
         await sub_manager(db_manager)
     except KeyboardInterrupt:
@@ -17,6 +18,6 @@ async def main():
         print(f"🛑 An error occurred: {e}")
     finally:
         await db_manager.close()
-    
+
 if __name__ == "__main__":
     asyncio.run(main())
